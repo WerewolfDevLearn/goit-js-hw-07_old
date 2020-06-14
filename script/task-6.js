c4164fa847ec509d0949ad33e98f6bc8cd075ac9
Mikhail <werewolfdevlearn@gmail.com>
2020-06-14 22:39:06 +0300

    Add task07

--- script/task-6.js
+++ script/task-6.js
@@ -1,12 +1,10 @@
-import users from './users.js';
+const inputRef = document.querySelector("input#validation-input");
+inputRef.addEventListener("input", validation);
 
-const getUsersWithAge = (users, min, max) => {
-  const usersWithAge = users.filter(user => user.age > min && user.age < max)
-  return usersWithAge
-};
-
-console.log(getUsersWithAge(users, 20, 30)); 
-// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
-
-console.log(getUsersWithAge(users, 30, 40));
-// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объектb
+function validation() {
+  if (inputRef.value.length >= 1 && inputRef.value.length <= 6) {
+    inputRef.classList.add("valid");
+  } else {
+    inputRef.classList.add("invalid");
+  }
+}
