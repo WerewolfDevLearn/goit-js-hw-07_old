b1965f2367872cef117b5e5ea9f5ed228b356c10
Mikhail <werewolfdevlearn@gmail.com>
2020-06-17 17:42:00 +0300

    Add task08

--- script/task-7.js
+++ script/task-7.js
@@ -1,7 +1,7 @@
 const barRef = document.querySelector("#font-size-control");
 
 const textRef = document.querySelector("#text");
-console.log(textRef.style.fontSize);
+// console.log(textRef.style.fontSize);
 // textRef.style.fontSize = "30px";
 barRef.addEventListener("input", () => {
   textRef.style.fontSize = `${barRef.value}px`;
--- script/task-8.js
+++ script/task-8.js
@@ -1,19 +1,43 @@
-import users from './users.js';
+const btnCreate = document.querySelector('button[data-action="render"]');
+const btnDestroy = document.querySelector('button[data-action="destroy"]');
+const inputNumber = document.querySelector("#controls input");
+const outputBox = document.querySelector("#boxes");
 
-console.log(users);
+let amount = 0;
+inputNumber.addEventListener("input", getInputValue);
+btnCreate.addEventListener("click", () => {
+  createBoxes(amount);
+});
 
-const getUsersWithFriend = (users, friendName) => {
-  const userWithFriend = users.reduce((acc, user) => {
-    if (user.friends.includes(friendName)) {
-      acc.push(user.name);
-    }
-    return acc;
-  }, []);
+btnDestroy.addEventListener("click", destroyBoxes);
 
-  return userWithFriend;
-};
+function createBoxes(amount) {
+  console.log("infunction", `${amount}`);
+  const itemsContainer = document.createElement("div");
+  itemsContainer.classList.add("container");
 
-console.log(getUsersWithFriend(users, 'Briana Decker'));
-// [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, 'Goldie Gentry'));
-// [ 'Elma Head', 'Sheree Anthony' ]
+  for (let i = 0; i <= amount; i += 1) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    item.style.backgroundColor = colorGenerator();
+    item.style.width = `${30 + 10 * i}px`;
+    item.style.height = `${30 + 10 * i}px`;
+    itemsContainer.append(item);
+  }
+  outputBox.append(itemsContainer);
+  return outputBox;
+}
+
+function destroyBoxes() {
+  const container = document.querySelector("div.container");
+  container.remove();
+}
+
+function colorGenerator() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+function getInputValue() {
+  amount = inputNumber.value;
+  console.log(amount);
+  return amount;
+}
